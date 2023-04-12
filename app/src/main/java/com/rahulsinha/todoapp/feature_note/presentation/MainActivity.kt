75d343c71276c11827eed6ad64307fae9aef96ba
Rahul Sinha <rahul@rapido.bike>
2023-04-12 17:48:38 +0530

    Resolved dependency injection issues

--- app/src/main/java/com/rahulsinha/todoapp/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/rahulsinha/todoapp/feature_note/presentation/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
